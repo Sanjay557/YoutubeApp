759b843ea23b6aba90dbe785e90e481bc8b9a0d8
Sanjay557 <60855947+Sanjay557@users.noreply.github.com>
2025-08-21 16:56:08 +0530

    How to save to imageKit is done.

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "./client";
+import ImageKit from "imagekit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -6,29 +7,56 @@ export const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
     return { message: `Hello ${event.data.email}!` };
-  },
+  }
 );
 
+const imageKit = new ImageKit({
+  //@ts-ignore
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  //@ts-ignore
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  //@ts-ignore
+  urlEndpoint: process.env.IMAGEKIT_URLENDPOINT,
+});
+
 export const GenerateAiThumbnail = inngest.createFunction(
-  {id: 'ai/generate-thumbnail'},
-  {event : 'ai/generate-thumbnail'},
-  async({event, step}) => {
-    const {userEmail, refImage, faceImage, userInput} = await event.data
+  { id: "ai/generate-thumbnail" },
+  { event: "ai/generate-thumbnail" },
+  async ({ event, step }) => {
+    const { userEmail, refImage, faceImage, userInput } = await event.data;
     //Upload Image to cloud /ImageKit
-    
-    
+
+    const uploadImageUrls = await step.run("UploadImage", async () => {
+      if (refImage != null) {
+        const refImageUrl = await imageKit.upload({
+          file: refImage?.buffer ?? "",
+          fileName: refImage.name,
+          isPublished: true,
+          useUniqueFileName: false,
+        });
+
+        // const faceImageUrl = await imageKit.upload({
+        //   file: faceImage?.buffer??'',
+        //   fileName: faceImage.name,
+        //   isPublished: true,
+        //   useUniqueFileName: false
+        // })
+
+        return refImageUrl.url;
+      }
+      else{
+        return null
+      }
+    });
+
     //Generate AI promopt from AI model
 
-    
     //Generate AI Image
-
 
     //Save Image to cloud
 
-
     //Save Record to Database
 
-    return userEmail
-
+    return uploadImageUrls;
   }
-)
+);
