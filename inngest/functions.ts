b556d6fc0aaf16586e90f0bc73c1dd3f53d490de
Sanjay557 <60855947+Sanjay557@users.noreply.github.com>
2025-08-22 14:58:54 +0530

    Now the prompt is getting generated.

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,5 +1,6 @@
 import { inngest } from "./client";
 import ImageKit from "imagekit";
+import OpenAI from 'openai';
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -19,13 +20,19 @@ const imageKit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URLENDPOINT,
 });
 
+export const openai = new OpenAI({
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.OPENROUTER_API_KEY,
+  
+});
+
 export const GenerateAiThumbnail = inngest.createFunction(
   { id: "ai/generate-thumbnail" },
   { event: "ai/generate-thumbnail" },
   async ({ event, step }) => {
     const { userEmail, refImage, faceImage, userInput } = await event.data;
+    
     //Upload Image to cloud /ImageKit
-
     const uploadImageUrls = await step.run("UploadImage", async () => {
       if (refImage != null) {
         const refImageUrl = await imageKit.upload({
@@ -50,13 +57,47 @@ export const GenerateAiThumbnail = inngest.createFunction(
     });
 
     //Generate AI promopt from AI model
+    const generateThumbnailPrompt = await step.run('generateThumbnailPrompt' , async()=> {
+      const completion = await openai.chat.completions.create({
+        model: 'google/gemini-2.5-flash',
+        messages: [
+          {
+            role: 'user',
+            content: [
+              {
+                "type": "text",
+                "text": uploadImageUrls? `Refering to this thumbnail url write a text prompt to generate Youtube Thumbnail
+                similar to the attached referance image with following user input:` + userInput + 'Only give me text prompt, No other comment text' :
+                `Depends on user input write a text prompt to generate high quality professional Youtube video
+                Add relevant icons, illustrations or images as per title userinput` + userInput + 'Only give me text prompt, No other comment text'
+                
+              },
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": uploadImageUrls?? ''
+              }
+            }
+            ]
+          },
+        ],
+        max_tokens: 300,
+      }); 
+      console.log(completion.choices[0].message.content)
+
+      return completion.choices[0].message.content
+    })
 
     //Generate AI Image
+    
 
     //Save Image to cloud
 
+
     //Save Record to Database
 
-    return uploadImageUrls;
+    return generateThumbnailPrompt;
+
+    //Run the server and check it
   }
 );
